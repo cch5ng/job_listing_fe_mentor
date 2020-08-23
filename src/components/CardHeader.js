ea813d62903707c9d91e268bee736d4ccde8c966
Carol Chung <cch5ng@gmail.com>
2020-08-23 12:08:31 -0700

    fix svg sizing for mobile display

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import '../App.scss';
 import Button from './Button';
-import { ReactComponent as Logo1 } from '../images/photosnap.svg';
-import { ReactComponent as Logo2 } from '../images/manage.svg';
-import { ReactComponent as Logo3 } from '../images/account.svg';
-import { ReactComponent as Logo4 } from '../images/myhome.svg';
-import { ReactComponent as Logo5 } from '../images/loop-studios.svg';
-import { ReactComponent as Logo6 } from '../images/faceit.svg';
-import { ReactComponent as Logo7 } from '../images/shortly.svg';
-import { ReactComponent as Logo8 } from '../images/insure.svg';
-import { ReactComponent as Logo9 } from '../images/eyecam-co.svg';
-import { ReactComponent as Logo10 } from '../images/the-air-filter-company.svg';
+//import { ReactComponent as Logo1 } from '../images/photosnap.svg';
+import logo1 from '../images/photosnap.svg';
+import logo2 from '../images/manage.svg';
+import logo3 from '../images/account.svg';
+import logo4 from '../images/myhome.svg';
+import logo5 from '../images/loop-studios.svg';
+import logo6 from '../images/faceit.svg';
+import logo7 from '../images/shortly.svg';
+import logo8 from '../images/insure.svg';
+import logo9 from '../images/eyecam-co.svg';
+import logo10 from '../images/the-air-filter-company.svg';
 
 function CardHeader(props) {
   const {company, logo, isNew, featured, position, postedAt, contract,
@@ -18,34 +19,34 @@ function CardHeader(props) {
 
   const renderLogo = () => {
     if (id === 1) {
-      return (<Logo1 title="company logo" className="logo" />)
+      return (<img src={logo1} style={{height: '50px'}} />)
     }
     if (id === 2) {
-      return (<Logo2 title="company logo" className="logo" />)
+      return (<img src={logo2} style={{height: '50px'}} />)
     }
     if (id === 3) {
-      return (<Logo3 />)
+      return (<img src={logo3} style={{height: '50px'}} />)
     }
     if (id === 4) {
-      return (<Logo4 />)
+      return (<img src={logo4} style={{height: '50px'}} />)
     }
     if (id === 5) {
-      return (<Logo5 />)
+      return (<img src={logo5} style={{height: '50px'}} />)
     }
     if (id === 6) {
-      return (<Logo6 />)
+      return (<img src={logo6} style={{height: '50px'}} />)
     }
     if (id === 7) {
-      return (<Logo7 />)
+      return (<img src={logo7} style={{height: '50px'}} />)
     }
     if (id === 8) {
-      return (<Logo8 />)
+      return (<img src={logo8} style={{height: '50px'}} />)
     }
     if (id === 9) {
-      return (<Logo9 />)
+      return (<img src={logo9} style={{height: '50px'}} />)
     }
     if (id === 10) {
-      return (<Logo10 />)
+      return (<img src={logo10} style={{height: '50px'}} />)
     }
   }
 
@@ -54,7 +55,7 @@ function CardHeader(props) {
       <div className="logo">
         {renderLogo()}
       </div>
-      <div className="row">
+      <div className="row card-line1">
         <div className="h2">{company}</div>
         {isNew === true && (
           <Button label="NEW!" type="new" />
