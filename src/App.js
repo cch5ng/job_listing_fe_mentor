7723b590de6e10a1ca345f4a0f7838993afaa705
Carol Chung <cch5ng@gmail.com>
2020-08-23 16:39:56 -0700

    fix footer styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
 import Card from './components/Card';
 import jobs from './data.json';
+import headerMobile from './images/bg-header-mobile.svg';
+import headerDesktop from './images/bg-header-desktop.svg';
+
+const styles = {
+  container: isDesktopSized => ({
+    backgroundImage: isDesktopSized ? `url(${headerDesktop})`: `url(${headerMobile})`
+  })
+};
 
 function App() {
-  console.log('jobs', jobs)
+  const mediaMatch = window.matchMedia('(min-width: 768px)');
+  const [matches, setMatches] = useState(false); //mediaMatch.matches
+
+  useEffect(() => {
+    const handler = e => setMatches(e.matches);
+    mediaMatch.addListener(handler);
+    return () => mediaMatch.removeListener(handler);
+  }, []);
 
   return (
-    <div className="App">
-      <header>
-        test primary color
-      </header>
+    <div>
+      <header style={styles.container(matches)} />
       <main id="app">
         {jobs.map(job => 
           (<Card key={job.id} job={job} />)
@@ -19,7 +32,7 @@ function App() {
       <footer>
         <div className="attribution">
           Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-          Coded by <a href="#">Your Name Here</a>.
+          Coded by <a href="http://github.com/cch5ng" target="_blank">Carol Chung</a>.
         </div>  
       </footer>
     </div>
